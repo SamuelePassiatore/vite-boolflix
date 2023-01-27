3c1b36be052667b2b51733c6a4b2bbe93c6db50e
SamuelePassiatore <semmisno2@protonmail.com>
2023-01-27 16:45:55 +0100

    add header component with emits

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script>
 import axios from 'axios';
 import { store } from './assets/data/store';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
   name: 'Boolflix',
+  components: { AppHeader, AppMain },
   data() {
     return {
       apiKey: 'api_key=4f01516c5f5e36c277e6c94fe530bbe4',
       apiUriMovie: `https://api.themoviedb.org/3/search/movie?api_key=4f01516c5f5e36c277e6c94fe530bbe4&query=`,
       apiUriSeries: `https://api.themoviedb.org/3/search/tv?api_key=4f01516c5f5e36c277e6c94fe530bbe4&query=`,
       store,
-      searchTerm: ''
     }
   },
   methods: {
@@ -17,79 +19,63 @@ export default {
       const url = new URL(`./assets/img/${image}.png`, import.meta.url);
       return url.href
     },
-
-    fetchMovie(url) {
+    searchMovie(url) {
       axios.get(url)
         .then((res) => {
           store.movies = res.data.results
         }).catch(error => { console.log(error) })
     },
-
-    fetchSeries(url) {
+    searchSeries(url) {
       axios.get(url)
         .then((res) => {
           store.series = res.data.results
         }).catch(error => { console.log(error) })
     },
-
-    onTypeSearch() {
-      const apiUriMovie = this.apiUriMovie + `${this.searchTerm}`;
-      this.fetchMovie(apiUriMovie);
-
-      const apiUriSeries = this.apiUriSeries + `${this.searchTerm}`;
-      this.fetchSeries(apiUriSeries);
+    onTypeSearch(searchTerm) {
+      const apiUriMovie = this.apiUriMovie + `${searchTerm}`;
+      this.searchMovie(apiUriMovie);
+      const apiUriSeries = this.apiUriSeries + `${searchTerm}`;
+      this.searchSeries(apiUriSeries);
     },
-
-    clearInput() {
-      this.searchTerm = '';
-    },
-
-
   },
-
 }
-
 </script>
 
+
 <template>
-  <font-awesome-icon icon="fa-solid fa-star" />
-  <div class="container">
-    <div class="input-group">
-      <input v-model.trim="this.searchTerm" @keyup.enter="onTypeSearch" @keyup.esc="clearInput" type="text"
-        placeholder="Inserisci un titolo" class="form-control">
-      <button @click="onTypeSearch" :class="{ disabled: !this.searchTerm }" class="btn btn-primary">Cerca</button>
-    </div>
+  <!-- <font-awesome-icon icon="fa-solid fa-star" />
+  <div class="container"> -->
+  <app-header @search-term="onTypeSearch"></app-header>
 
-    <div class="movies">
-      <h1>Film</h1>
-      <ul class="list-group" v-for="movie in store.movies" @key="movies.id">
-        <li class="list-group-item"><img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`"
-            :alt="movie.title" class="img-fluid">
-        </li>
-        <li class="list-group-item">Titolo: {{ movie.title }}</li>
-        <li class="list-group-item">Titolo Originale: {{ movie.original_title }}</li>
-        <li class="list-group-item">Lingua: <img class="img-fluid flag" :src="getImagePath(movie.original_language)"
-            :alt="movie.title"></li>
-        <li class="list-group-item">Voto: {{ movie.vote_average }}</li>
-      </ul>
-    </div>
-
-    <div class="series">
-      <h1>Serie TV</h1>
-      <ul class="list-group" v-for="serie in store.series" @key="series.id">
-        <li class="list-group-item"><img :src="`https://image.tmdb.org/t/p/w342/${serie.poster_path}`" :alt="serie.name"
-            class="img-fluid">
-        </li>
-        <li class="list-group-item">Titolo: {{ serie.name }}</li>
-        <li class="list-group-item">Titolo Originale: {{ serie.original_name }}</li>
-        <li class="list-group-item">Lingua: <img class="img-fluid flag" :src="getImagePath(serie.original_language)"
-            :alt="serie.name"></li>
-        <li class="list-group-item">Voto: {{ serie.vote_average }}</li>
-      </ul>
-    </div>
+  <!-- <div class="movies">
+    <h1>Film</h1>
+    <ul class="list-group" v-for="movie in store.movies" @key="movies.id">
+      <li class="list-group-item"><img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" :alt="movie.title"
+          class="img-fluid">
+      </li>
+      <li class="list-group-item">Titolo: {{ movie.title }}</li>
+      <li class="list-group-item">Titolo Originale: {{ movie.original_title }}</li>
+      <li class="list-group-item">Lingua: <img class="img-fluid flag" :src="getImagePath(movie.original_language)"
+          :alt="movie.title"></li>
+      <li class="list-group-item">Voto: {{ movie.vote_average }}</li>
+    </ul>
   </div>
-</template>
+  <div class="series">
+    <h1>Serie TV</h1>
+    <ul class="list-group" v-for="serie in store.series" @key="series.id">
+      <li class="list-group-item"><img :src="`https://image.tmdb.org/t/p/w342/${serie.poster_path}`" :alt="serie.name"
+          class="img-fluid">
+      </li>
+      <li class="list-group-item">Titolo: {{ serie.name }}</li>
+      <li class="list-group-item">Titolo Originale: {{ serie.original_name }}</li>
+      <li class="list-group-item">Lingua: <img class="img-fluid flag" :src="getImagePath(serie.original_language)"
+          :alt="serie.name"></li>
+      <li class="list-group-item">Voto: {{ serie.vote_average }}</li>
+    </ul>
+  </div> -->
 
+  <!-- </div> -->
+</template>
 <style lang="scss" scoped>
 @use './assets/scss/style.scss'
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,14 @@
 <script>
+import { store } from '../assets/data/store'
 export default {
     name: 'AppHeader',
+    data() {
+        return {
+            searchTerm: '',
+            store
+        }
+    },
+    emits: ['search-term']
 
 
 }
@@ -8,9 +16,21 @@ export default {
 </script>
 
 <template>
-
+    <header class="d-flex align-items-center">
+        <h1>BOOLFLIX</h1>
+        <div class="input-group d-flex justify-content-end">
+            <input v-model.trim="searchTerm" @keyup.enter="$emit('search-term', searchTerm)" type="text"
+                placeholder="Inserisci un titolo">
+            <button @click="$emit('search-term', searchTerm)" :class="{ disabled: !searchTerm }"
+                class="btn btn-primary">Cerca</button>
+        </div>
+    </header>
 </template>
 
 <style lang="scss" scoped>
+header {
+    height: 100px;
+    background-color: red;
 
+}
 </style>
